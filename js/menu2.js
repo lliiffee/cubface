320ffeccfcbb403a5cafb387c2e99c0df4ce63f1
lliiffee <lliiffee.feng@gmail.com>
2014-02-13 17:19:18 +0800

    锋3

---
+++ js/menu2.js
@@ -0,0 +1,34 @@
+
+$(document).ready(function(){
+var menu2= '<div class="container">'
++  '<div class="row">'
++		            '<article class="col-lg-12 col-md-12 col-sm-12">'
++		                '<div class="menuBox clearfix">'
++		                   ' <nav class="navbar navbar-default navbar-static-top tm_navbar clearfix" role="navigation">'
++		                        '<ul class="nav sf-menu clearfix">'
++		                            '<li class="active"><a href="#">首页1</a>'  
++		                            '</li>'
++		                            '<li class="sub-menu"><a href="#">关于我们<span></span></a>'
++		                            	'<ul class="submenu">'
++		                                    '<li><a href="aboutus/index.html">企业简介</a></li>'
++		                                    '<li><a href="aboutus/history.html">发展历程</a></li>'
++		                                    '<li><a href="aboutus/index.html">品牌介绍 </a></li>'
++		                                    '<li><a href="aboutus/store.html">店铺分布 </a></li>'
++		                                    '<li><a href="aboutus/team.html">团队介绍 </a></li>'
++		                               '</ul>'
++		                            '</li>'
++		                            '<li><a href="cooperation/index.html">合作范畴</a></li>'
++		                            '<li><a href="collection/cubface/index.html">产品展示</a></li>'
++		                            '<li><a href="pressroom/index.html">新闻中心</a></li>'
++		                            '<li><a href="aftersale/index.html">售后服务</a></li>'
++		                            '<li><a href="contactus/index.html">招聘信息</a></li>'
++		                            '<li><a href="mall/index.html">在线商城</a></li>'
++		                        '</ul>'
++		                  '  </nav>'
++		               ' </div>'
++		            '</article>'
++		       ' </div>'
++		    '</div>';
+
+ $('#sub_menu').append(menu2);
+});
